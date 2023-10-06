9e9b4cc4826ff7baa9e1b42192d10a982dc4ed22
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-10-06 10:15:45 -0500

    added findByName function

--- src/main/java/com/MbkEcommerce/dao/ProductRepository.java
+++ src/main/java/com/MbkEcommerce/dao/ProductRepository.java
@@ -12,5 +12,11 @@ import org.springframework.data.domain.Pageable;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+
+    // SELECT * FROM Product p
+    //WHERE
+    // p.name LIKE CONCAT ('%', :name, ''%)
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
 }
 

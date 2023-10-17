b4e53647dd536fd37dc30d70330af4d66f902288
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-10-17 08:39:14 -0500

    added cart-status component

--- src/main/ui-angular/src/app/components/product-list/product-list.component.ts
+++ src/main/ui-angular/src/app/components/product-list/product-list.component.ts
@@ -125,6 +125,14 @@ export class ProductListComponent implements OnInit {
     }
 
 
+    addToCart(theProduct: Product){
+
+      console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`)
+
+
+    }
+
+
 }
 
 

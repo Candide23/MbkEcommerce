40c8feeea8fc0966438a724ec8c0879c7c1ca4a7
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-10-18 07:33:10 -0500

    add cart from details view

--- src/main/ui-angular/src/app/components/cart-status/cart-status.component.ts
+++ src/main/ui-angular/src/app/components/cart-status/cart-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
@@ -7,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartStatusComponent implements OnInit {
 
-  constructor () {}
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor (private cartService: CartService) {}
 
 
   ngOnInit(): void {
+    this.updateCartStatus();
+
+  }
+  updateCartStatus() {
+
+    // subcribe to the cart totalPrice
+
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subcribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
 
   }
 
--- src/main/ui-angular/src/app/components/product-list/product-list.component.ts
+++ src/main/ui-angular/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-list',
@@ -25,6 +27,7 @@ export class ProductListComponent implements OnInit {
   previousKeywor: string = "";
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -128,6 +131,10 @@ export class ProductListComponent implements OnInit {
     addToCart(theProduct: Product){
 
       console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`)
+
+      const theCartItem = new CartItem(theProduct);
+
+      this.cartService.addToCart(theCartItem);
 
 
     }

2b628c927b19e1f1dc1cb114b96389a6d8effdb1
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-10-19 11:42:17 -0500

    added list shopping cart items

--- src/main/ui-angular/src/app/app.module.ts
+++ src/main/ui-angular/src/app/app.module.ts
@@ -13,8 +13,10 @@ import { ProductsDetailsComponent } from './components/products-details/products
 
 import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes : Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductsDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
@@ -33,7 +35,8 @@ const routes : Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductsDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),

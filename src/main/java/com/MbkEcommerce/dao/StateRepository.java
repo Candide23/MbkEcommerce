e5e72a367767b1adf62983fbc8ed52687c8911f6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-10-30 09:06:42 -0500

    added state repository

---
+++ src/main/java/com/MbkEcommerce/dao/StateRepository.java
@@ -0,0 +1,12 @@
+package com.MbkEcommerce.dao;
+
+import com.MbkEcommerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+}
--- src/main/java/com/MbkEcommerce/entity/Country.java
+++ src/main/java/com/MbkEcommerce/entity/Country.java
@@ -1,2 +1,32 @@
-package com.MbkEcommerce.entity;public class Country {
+package com.MbkEcommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+//import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
 }

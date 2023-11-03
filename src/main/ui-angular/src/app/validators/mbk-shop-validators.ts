f76c75e8ad134cafef11381a2b108d102db6a0a8
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-11-03 08:34:03 -0500

    added shipping validation

--- src/main/ui-angular/src/app/validators/mbk-shop-validators.ts
+++ src/main/ui-angular/src/app/validators/mbk-shop-validators.ts
@@ -2,18 +2,17 @@ import { FormControl, ValidationErrors } from "@angular/forms";
 
 export class MbkShopValidators {
 
-    // whitespce validation
+    // whitespace validation
     static notOnlyWhitespace(control: FormControl): ValidationErrors {
 
-        // check if string only contains whitespce
-
+        // check if string is not empty and does not contain only whitespace
         if((control.value != null) && (control.value.trim().length === 0)) {
             return { 'notOnlyWhitespace': true };
         }
         else {
-            // vaid, retrun null
+            // return null if validation passes
             return null;
-        }      
+        }
 
     }
 

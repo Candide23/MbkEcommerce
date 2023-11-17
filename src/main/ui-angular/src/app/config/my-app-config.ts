f6ad27657198bf5eab22a551f5090796670f28b9
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-11-17 10:11:34 -0600

    created OIDC client App

---
+++ src/main/ui-angular/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default  {
+
+    oidc: {
+        clientId: '0oadbv5y2cavlcGbc5d7',
+        issuer: 'https://dev-15777272.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}

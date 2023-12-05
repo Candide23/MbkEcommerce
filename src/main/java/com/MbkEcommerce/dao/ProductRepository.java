f1339b258993dde7f13ccf7dfe283ee36fa087eb
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-12-05 10:56:21 -0600

    refactoring backend configs

--- src/main/java/com/MbkEcommerce/dao/ProductRepository.java
+++ src/main/java/com/MbkEcommerce/dao/ProductRepository.java
@@ -4,10 +4,12 @@ import com.MbkEcommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.data.domain.Pageable;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);

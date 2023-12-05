dc73faf674e8e696c1cc48e4c3c3667edf6106fc
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-12-05 10:10:45 -0600

    configured restcontroller

--- src/main/java/com/MbkEcommerce/controller/CheckoutController.java
+++ src/main/java/com/MbkEcommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.MbkEcommerce.dto.PurchaseResponse;
 import com.MbkEcommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {

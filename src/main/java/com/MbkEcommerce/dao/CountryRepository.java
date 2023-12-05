b052dba99329cfc9973dba5754c5220750953794
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-12-05 10:03:32 -0600

    configured CORS mapping

--- src/main/java/com/MbkEcommerce/dao/CountryRepository.java
+++ src/main/java/com/MbkEcommerce/dao/CountryRepository.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.swing.plaf.nimbus.State;
 
-@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
--- src/main/java/com/MbkEcommerce/dao/ProductRepository.java
+++ src/main/java/com/MbkEcommerce/dao/ProductRepository.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.data.domain.Pageable;
 
-@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
